b56022cb150e888365b5def1396024b162ec8dfc
LAPTOP-TOGP7PJ0\azigic <azigic@ztech.io>
2020-08-10 13:22:07 +0200

    added appspec.yml

---
+++ instcdagent.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -x
+sudo yum -y update
+sudo yum -y install ruby
+sudo yum -y install wget
+sudo yum -y install httpd
+cd /home/ec2-user
+wget https://aws-codedeploy-eu-west-1.s3.eu-west-1.amazonaws.com/latest/install
+sudo chmod +x ./install
+sudo ./install auto
